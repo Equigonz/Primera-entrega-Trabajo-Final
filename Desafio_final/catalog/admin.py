98ca3e1cff9602e6400486a1b4c08292381341d3
Ezequiel Gonzalez <105887298+Equigonz@users.noreply.github.com>
2022-08-19 21:08:22 -0300

    Update admin.py

--- Desafio_final/catalog/admin.py
+++ Desafio_final/catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from catalog.models import Books
+from catalog.models import Ebooks
+from catalog.models import Audiobooks
 # Register your models here.
+
+@admin.register(Audiobooks)
+class Audiobooks_admin(admin.ModelAdmin):
+    list_display = ["title", "book_genre"]
+
+@admin.register(Books)
+class Books_admin(admin.ModelAdmin):
+    list_display = ["title", "book_genre"]
+    
+@admin.register(Ebooks)
+class Ebooks_admin(admin.ModelAdmin):
+    list_display = ["title", "book_genre"] 
