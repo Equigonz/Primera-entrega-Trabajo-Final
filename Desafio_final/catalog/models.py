5683c4c45202c9853b68c6b3de87854d99016007
Ezequiel Gonzalez <105887298+Equigonz@users.noreply.github.com>
2022-08-19 21:09:00 -0300

    Update models.py

--- Desafio_final/catalog/models.py
+++ Desafio_final/catalog/models.py
@@ -6,10 +6,19 @@ class Books(models.Model):
     title = models.CharField(max_length=120)
     book_genre = models.CharField(max_length=40)   
 
+    def __str__(self):
+        return self.title
+
 class Ebooks(models.Model):
     title = models.CharField(max_length=120)
     book_genre = models.CharField(max_length=40)
-        
+
+    def __str__(self):
+        return self.title
+            
 class Audiobooks(models.Model):
     title = models.CharField(max_length=120)
     book_genre = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.title
