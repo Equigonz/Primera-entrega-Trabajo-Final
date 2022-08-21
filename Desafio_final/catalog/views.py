9758e5123dfc9176409c30d1c4e06743cf731bef
FabiiAndres10 <f.vegasilva97@hotmail.com>
2022-08-21 19:56:53 -0400

    update final

--- Desafio_final/catalog/views.py
+++ Desafio_final/catalog/views.py
@@ -121,7 +121,15 @@ def delete_audiobook(request, pk):
 
 def update_book(request, pk):
     if request.method == 'POST':
-        pass
+        
+        form_book = Formulario_create_book(request.POST)
+        if form_book.is_valid():
+            book = Books.objects.get(id=pk)
+            book.title = form_book.cleaned_data['title']
+            book.book_genre = form_book.cleaned_data['book_genre']
+            book.save()
+
+            return redirect(list_book)
 
     elif request.method == 'GET':
         book = Books.objects.get(id=pk)
@@ -133,7 +141,15 @@ def update_book(request, pk):
 
 def update_ebook(request, pk):
     if request.method == 'POST':
-        pass
+        
+        form_ebook = Formulario_create_ebook(request.POST)
+        if form_ebook.is_valid():
+            ebook = Ebooks.objects.get(id=pk)
+            ebook.title = form_ebook.cleaned_data['title']
+            ebook.book_genre = form_ebook.cleaned_data['book_genre']
+            ebook.save()
+
+            return redirect(list_ebook)
 
     elif request.method == 'GET':
         ebook = Ebooks.objects.get(id=pk)
@@ -145,12 +161,20 @@ def update_ebook(request, pk):
 
 def update_audiobook(request, pk):
     if request.method == 'POST':
-        pass
+        
+        form_audiobook = Formulario_create_audiobook(request.POST)
+        if form_audiobook.is_valid():
+            audiobook = Audiobooks.objects.get(id=pk)
+            audiobook.title = form_audiobook.cleaned_data['title']
+            audiobook.book_genre = form_audiobook.cleaned_data['book_genre']
+            audiobook.save()
+
+            return redirect(list_audiobook)
 
     elif request.method == 'GET':
         audiobook = Audiobooks.objects.get(id=pk)
     
-        form_audiobook = Formulario_create_ebook(initial={'title':audiobook.title, 'book_genre':audiobook.book_genre})
+        form_audiobook = Formulario_create_audiobook(initial={'title':audiobook.title, 'book_genre':audiobook.book_genre})
         context = {'form_audiobook': form_audiobook}
         
         return render(request, 'update_audiobook.html', context = context)
